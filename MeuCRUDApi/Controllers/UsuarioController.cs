b9c23ad3140de8bfd85ffc4a858ce621869a8d47
Lucca C. L. Z. Sandoval <luccasandoval9@gmail.com>
2025-04-05 20:03:38 -0300

    Criado o CRUD de Usuarios (CREATE,READ,UPDATE,DELETE)

--- MeuCRUDApi/Controllers/UsuarioController.cs
+++ MeuCRUDApi/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MeuCRUDApi.Models;
+using MeuCRUDApi.Repositorios.Interfaces;
 
 namespace MeuCRUDApi.Controllers;
 
@@ -9,9 +10,52 @@ namespace MeuCRUDApi.Controllers;
 
 public class UsuarioController : ControllerBase
 {
-    [HttpGet]
-    public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+
+    private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+    public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
     {
-        return Ok();
+        _usuarioRepositorio = usuarioRepositorio;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios()
+    {
+        List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
+        return Ok(usuarios);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult <UsuarioModel>> BuscarPorId(int id)
+    {
+        UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
+        return Ok(usuario);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+    {
+        UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+
+        return Ok(usuario);
+    } 
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel,int id)
+    {
+        usuarioModel.Id = id;
+        UsuarioModel usuario = await _usuarioRepositorio.Atualizar(usuarioModel, id);
+
+        return Ok(usuario);
+    } 
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+    {
+    
+        bool apagado = await _usuarioRepositorio.Apagar(id);
+
+        return Ok(apagado);
+    } 
+
 }
